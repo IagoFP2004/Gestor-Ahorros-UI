72902e42e7a679bba523eb5e8bedfcb7b284a197
IagoFP <iagofranciscoperez123@gmail.com>
2025-12-02 12:31:53 +0100

    Ajustado Responsive

--- src/components/Mes.jsx
+++ src/components/Mes.jsx
@@ -5,24 +5,28 @@ export function Mes() {
                 <h5>Diciembre 2025</h5>
                 <i class="bi bi-trash text-danger"></i>
             </div>
-            <div className="d-flex flex-row justify-content-between mt-4 text-center">
-                <div>
+            <div className="row mt-4 text-center">
+                <div className="col-6 col-md-3">
                     <p>Salario:</p>
                     <p>$0</p>
                 </div>
-                <div>
+
+                <div className="col-6 col-md-3">
                     <p>Gastos:</p>
                     <p>$0</p>
                 </div>
-                <div>
+
+                <div className="col-6 col-md-3">
                     <p>Ahorros</p>
                     <p>$0</p>
                 </div>
-                <div>
+
+                <div className="col-6 col-md-3">
                     <p>Restante</p>
                     <p className="text-success">$0</p>
                 </div>
             </div>
+
         </div>
     );
 }
